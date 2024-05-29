46d0010ef57f67340ff28b56325fb299a44fdccb
Shreysid <Shreysid2352@gmail.com>
2024-05-29 12:25:14 +0530

    Q&A app

---
+++ Question-app/Question-app/Question_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_appApp.swift
+//  Question-app
+//
+//  Created by Shrey Sid on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Question_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
